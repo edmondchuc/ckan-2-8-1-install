59942df44bcecab416631280fada7e0bdad958e3
Edmond Chuc <edmond.chuc@csiro.au>
2018-11-28 13:37:02 +1000

    Added some package update commands

--- ckan-deployment/ckan-deploy-to-production.sh
+++ ckan-deployment/ckan-deploy-to-production.sh
@@ -1,7 +1,9 @@
 # This is script is required to run in the current folder named ckan-deployment as it requires the relative paths to some files like the app.wsgi
 
 # Install the required packages
-sudo apt-get install apache2 libapache2-mod-wsgi
+sudo apt update
+sudo apt ugprade -y
+sudo apt install apache2 libapache2-mod-wsgi
 
 # Copy the existing CKAN development configuration file and name it production.ini
 cp /etc/ckan/default/development.ini /etc/ckan/default/production.ini
