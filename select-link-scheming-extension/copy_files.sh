077cd4d81b1e3abe0bdcf9cf2be8fc9f2b677f74
Edmond Chuc <37032744+edmondchuc@users.noreply.github.com>
2018-11-30 13:18:53 +1000

    updated script to move form templates

--- select-link-scheming-extension/copy_files.sh
+++ select-link-scheming-extension/copy_files.sh
@@ -9,3 +9,7 @@ echo Done
 printf 'Copying select-link.html ... '
 sudo cp select-link.html /usr/lib/ckan/default/src/ckanext-scheming/ckanext/scheming/templates/scheming/display_snippets/
 echo Done
+
+printf 'Copying form-snippet-select.html ... '
+sudo cp form-snippet-select.html /usr/lib/ckan/default/src/ckanext-scheming/ckanext/scheming/templates/scheming/form_snippets/
+echo Done
