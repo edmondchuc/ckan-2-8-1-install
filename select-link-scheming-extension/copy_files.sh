7a58082feeebef1cf4e9a5b72cdf150a28c5c5ba
Edmond Chuc <37032744+edmondchuc@users.noreply.github.com>
2018-11-30 12:24:11 +1000

    Updated script

--- select-link-scheming-extension/copy_files.sh
+++ select-link-scheming-extension/copy_files.sh
@@ -2,9 +2,10 @@
 
 # This script copies the templates in the current directory to the required directories in CKAN.
 
-printf 'Copying form-select ...'
-sudo cp form-select.html /usr/lib/ckan/default/src/ckanext-scheming/ckanext/scheming/templates/scheming/form_snippets/
+printf 'Copying form-select.html ...'
+sudo cp form-select.html /usr/lib/ckan/default/src/ckan/ckan/templates/macros/
 echo Done
 
-printf 'Please manually copy the content in select-link.html to /usr/lib/ckan/default/src/ckan/ckan/templates/macros/'
-read
+printf 'Copying select-link.html '
+sudo cp select-link.html /usr/lib/ckan/default/src/ckanext-scheming/ckanext/scheming/templates/scheming/display_snippets/
+echo Done
