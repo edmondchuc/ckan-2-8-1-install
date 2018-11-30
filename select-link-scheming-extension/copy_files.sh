6eafb3a54241f6f84ac00f5972c60babf281bfb7
Edmond Chuc <37032744+edmondchuc@users.noreply.github.com>
2018-11-30 12:56:01 +1000

    edit

--- select-link-scheming-extension/copy_files.sh
+++ select-link-scheming-extension/copy_files.sh
@@ -6,6 +6,6 @@ printf 'Copying form-select.html ...'
 sudo cp form-select.html /usr/lib/ckan/default/src/ckan/ckan/templates/macros/
 echo Done
 
-printf 'Copying select-link.html '
+printf 'Copying select-link.html ... '
 sudo cp select-link.html /usr/lib/ckan/default/src/ckanext-scheming/ckanext/scheming/templates/scheming/display_snippets/
 echo Done
